65daea24515f0ab7b15d270bcf732a70f0428717
FileEX <alkom@t.pl>
2019-05-08 01:06:17 +0200

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alkom
+ * Date: 29.04.2019
+ * Time: 22:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace bytelua
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(162, 357);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(385, 28);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Compile";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(33, 45);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(612, 285);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(33, 17);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(149, 22);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Example 1";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(271, 17);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(149, 22);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Example 2";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(496, 17);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(149, 22);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "Example 3";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(673, 397);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "LUA OBFUSCATOR";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alkom
+ * Date: 29.04.2019
+ * Time: 22:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text;
+using MoonSharp.Interpreter;
+
+namespace bytelua
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{	
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void ObfuscateCode()
+		{
+			string scriptCode = @"    
+				x = '';
+				
+				bin = x;
+				local fb = '';
+				local dec
+				
+				function compileScript()
+					for i = 1, string.len(bin) do
+						dec, _ = ('\\%3d'):format(bin:sub(i,i):byte()):gsub(' ', '0');
+						fb = fb..dec;
+					end
+					
+					return fb;
+				end
+			";
+			
+			Script script = new Script();    
+			script.DoString(scriptCode);
+			
+			script.Globals["x"] = "loadstring(\"" + textBox1.Text + "\")";
+			script.Globals["bin"] = script.Globals["x"];
+			
+			DynValue res = script.Call(script.Globals["compileScript"]);
+			
+			textBox1.Text = "loadstring(\"" + res.String + "\")()";
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			ObfuscateCode();
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "_G['setTimer'](function()\r\n\tlocal i = 1\r\n\t_G['outputChatBox']('Hello world '..x)\r\n\ti = i +1\r\nend,1000,5)";
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "_G['addEventHandler']('onPlayerChat', root, function(msg, type)\r\n\tprint(_G['getPlayerName'](source), msg, type)\r\nend)";
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "_G['bindKey']('n','down',function(k,ks)\r\n\t_G['outputChatBox']('Your name: '.._G['getPlayerName'](_G['localPlayer']))\r\nend)\r\n\r\n_G['addEventHandler']('onClientResourceStart', resourceRoot, function()\r\n\t_G['outputChatBox']('Press b key')\r\nend)";
+		}
+	}
+}
